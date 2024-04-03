90a4e2b305e38a2eefa30cc3eb51730305608a38
LABORATORIO A <LABORATORIO A@LAB.EDU.BO>
2024-04-03 10:17:47 -0800

    Oji

--- Actividad3_4/Actividad.VISTA/Program.cs
+++ Actividad3_4/Actividad.VISTA/Program.cs
@@ -11,7 +11,7 @@ namespace Actividad.VISTA
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ClienteVista.ClienteInterfaz());
+            Application.Run(new PedidoVista.PedidoInterfaz());
         }
     }
 }
